22f8e4e553ad4e8410fde640c3f35a1e9c449ad6
NourHawaslH <110874656+NourHawaslH@users.noreply.github.com>
2022-08-09 00:53:45 -0400

    Add files via upload

---
+++ 100 quiz.py
@@ -0,0 +1,704 @@
+print('welcome to my computer quiz!')
+
+playing = input("do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("okay! lets play ;)")
+score = 0
+answer = input("What is the supreme law of the land? ")
+if answer == 'The Constitution':
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What does the Constitution do? ")
+if answer == "Sets up the government, Defines the government, Protects basic rights of Americans":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("The idea of self-government is in the first three words of the Constitution. What are these words? ")
+if answer == 'We the People':
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is an amendment? ")
+if answer == "A change to the Constitution, An addition to the Constitution":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What do we call the first ten amendments to the Constitution? ")
+if answer == "The Bill of Rights":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is one right or freedom from the First Amendment? ")
+if answer == "Speech, Religion, Assembly, Press, Petition the government":
+     print('correct!')
+     score += 1
+else:
+    print("incorrect!")
+
+answer = input("How many amendments does the Constitution have? ")
+if answer == "Twenty-seven (27)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What did the Declaration of Independence do? ")
+if answer == "Announced our independence (from Great Britain), Declared our independence (from Great Britain), Said that the United States is free (from Great Britain)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input(" What are two rights in the Declaration of Independence? ")
+if answer == "Life, Liberty, Pursuit of happiness":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is freedom of religion? ")
+if answer == "You can practice any religion or not practice a religion":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is the economic system in the United States? ")
+if answer == "Capitalist economy, Market economy":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is the “rule of law? ")
+if answer == "Everyone must follow the law, Leaders must obey the law, Government must obey the law, No one is above the law":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Name one branch or part of the government? ")
+if answer == "Congress":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What stops one branch of government from becoming too powerful? ")
+if answer == "Checks and balances, Separation of powers":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who is in charge of the executive branch? ")
+if answer == "The President":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input(" Who makes federal laws? ")
+if answer == "Congress, Senate and House of Representatives, U.S. or national legislature":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What are the two parts of the U.S. Congress? ")
+if answer == "The Senate and House of Representatives":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("How many U.S. Senators are there? ")
+if answer == "One hundred (100)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("We elect a U.S. Senator for how many years? ")
+if answer == "Six (6)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who is one of your state’s U.S. Senators now? ")
+if answer == "Answers will vary depending on the state in which you live. [District of Columbia residents and residents of U.S. territories should answer that D.C. (or the territory where the applicant lives) has no U.S. Senators.]":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("The House of Representatives has how many voting members? ")
+if answer == "Four hundred thirty-five (435)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("We elect a U.S. Representative for how many years? ")
+if answer == "Two (2)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Name your U.S. Representative. ")
+if answer == "Answers will vary depending on the district in which you live. [Residents of territories with nonvoting Delegates or Resident Commissioners may provide the name of that Delegate or Commissioner. Also acceptable is any statement that the territory has no (voting) Representatives in Congress.]":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who does a U.S. Senator represent? ")
+if answer == "All people of the state":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Why do some states have more Representatives than other states? ")
+if answer == "(Because of) the state’s , (Because) they have more people, (Because) some states have more people":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("We elect a President for how many years? ")
+if answer == "Four (4)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("In what month do we vote for President? ")
+if answer == "November":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is the name of the President of the United States now? ")
+if answer == "joe biden":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is the name of the Vice President of the United States now? ")
+if answer == "The Vice President":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("If both the President and the Vice President can no longer serve, who becomes President? ")
+if answer == "the Speaker of the House":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who is the Commander in Chief of the military? ")
+if answer == "The President":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input(" Who signs bills to become laws? ")
+if answer == "The President":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who vetoes bills? ")
+if answer == "The President":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What does the President’s Cabinet do? ")
+if answer == "Advises the President":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What are two Cabinet-level positions? ")
+if answer == "Secretary of Agriculture, Secretary of Commerce":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What does the judicial branch do? ")
+if answer == "Reviews laws, Explains laws, Resolves disputes (disagreements), Decides if a law goes against the Constitution":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is the highest court in the United States? ")
+if answer == "The Supreme Courts":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("How many justices are on the Supreme Court? ")
+if answer == "7":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who is the Chief Justice of the United States now? ")
+if answer == "john roberts":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who is the Governor of your state now? ")
+if answer == "greg abbot":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is the capital of your state? ")
+if answer == "austin":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What are the two major political parties in the United States? ")
+if answer == "Democratic and Republican":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is the political party of the President now? ")
+if answer == "democratic":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is the name of the Speaker of the House of Representatives now? ")
+if answer == "nancy pelosi":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("There are four amendments to the Constitution about who can vote. Describe one of them. ")
+if answer == "Any citizen can vote (Women and men can vote)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input(" What is one responsibility that is only for United States citizens? ")
+if answer == "Serve on a jury":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Name one right only for United States citizens. ")
+if answer == "Run for federal office":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What are two rights of everyone living in the United States? ")
+if answer == "Freedom of expression, Freedom of speech, Freedom of assembly, Freedom to petition the government, Freedom of religion, The right to bear arms":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What do we show loyalty to when we say the Pledge of Allegiance? ")
+if answer == "The United States":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is one promise you make when you become a United States citizen? ")
+if answer == "Give up loyalty to other countries, Defend the Constitution and laws of the United States, Obey the laws of the United States, Serve in the US military (if needed), Serve (do important work for) the nation (if needed), Be loyal to the United States":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("How old do citizens have to be to vote for President? ")
+if answer == "18":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What are two ways that Americans can participate in their democracy? ")
+if answer == "Vote, Join a political party":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("When is the last day you can send in federal income tax forms? ")
+if answer == "April 15":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("When must all men register for the Selective Service? ")
+if answer == "18-26":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is one reason colonists came to America? ")
+if answer == "freedom":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who lived in America before the Europeans arrived? ")
+if answer == "Native Americans":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input(" What group of people was taken to America and sold as slaves? ")
+if answer == "Africans":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Why did the colonists fight the British? ")
+if answer == "Because of high taxes (taxation without representation), Because the British army stayed in their houses (boarding, quartering), Because they didn’t have self-government":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who wrote the Declaration of Independence? ")
+if answer == "(Thomas) Jefferson":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("When was the Declaration of Independence adopted? ")
+if answer == "July 4, 1776":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("There were 13 original states. Name three. ")
+if answer == "North Carolina, South Carolina, Georgia":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What happened at the Constitutional Convention? ")
+if answer == "The Constitution was written":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("When was the Constitution written? ")
+if answer == "1787":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("The Federalist Papers supported the passage of the U.S. Constitution. Name one of the writers. ")
+if answer == "(James) Madison, (Alexander) Hamilton, (John) Jay, Publius":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is one thing Benjamin Franklin is famous for? ")
+if answer == "U.S. diplomat, Oldest member of the Constitutional Convention, First Postmaster General of the United States, Writer of “Poor Richard’s Almanac”,  Started the first free libraries":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who is the “Father of Our Country”? ")
+if answer == "(George) Washington":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who was the first President? ")
+if answer == "(George) Washington":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who was the first President ")
+if answer == "The Louisiana Territory, Louisiana":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Name one war fought by the United States in the 1800s. ")
+if answer == "War of 1812":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Name the U.S. war between the North and the South. ")
+if answer == "The Civil War":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Name one problem that led to the Civil War. ")
+if answer == "Slavery":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What was one important thing that Abraham Lincoln did? ")
+if answer == "Freed the slaves":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input(" What did the Emancipation Proclamation do? ")
+if answer == "Freed the slaves":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What did Susan B. Anthony do? ")
+if answer == "Fought for women’s rights":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Name one war fought by the United States in the 1900s. ")
+if answer == "World War I":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who was President during World War I? ")
+if answer == "(Woodrow) Wilson":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who was President during the Great Depression and World War II? ")
+if answer == "(Franklin) Roosevelt":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Who did the United States fight in World War II? ")
+if answer == "Japan, Germany, and Italy":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Before he was President, Eisenhower was a general. What war was he in? ")
+if answer == "World War II":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("During the Cold War, what was the main concern of the United States? ")
+if answer == "Communism":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What movement tried to end racial discrimination? ")
+if answer == "Civil rights (movement)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What did Martin Luther King, Jr. do? ")
+if answer == "Fought for civil rights":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What major event happened on September 11, 2001, in the United States? ")
+if answer == "Terrorists attacked the United States":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input(" Name one American Indian tribe in the United States. ")
+if answer == "Cherokee":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Name one of the two longest rivers in the United States. ")
+if answer == "Missouri (River)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What ocean is on the West Coast of the United States? ")
+if answer == "Pacific (Ocean)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What ocean is on the East Coast of the United States? ")
+if answer == "Atlantic (Ocean)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Name one U.S. territory. ")
+if answer == "Puerto Rico":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input(" Name one state that borders Canada. ")
+if answer == "Maine":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Name one state that borders Mexico. ")
+if answer == "California":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is the capital of the United States? ")
+if answer == "Washington, D.C.":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Where is the Statue of Liberty? ")
+if answer == "New York (Harbor)":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Why does the flag have 13 stripes? ")
+if answer == "Because there were 13 original colonies":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Why does the flag have 50 stars? ")
+if answer == "Because there is one star for each state":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What is the name of the national anthem? ")
+if answer == "The Star-Spangled Banner":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("When do we celebrate Independence Day? ")
+if answer == "July 4":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("Name two national U.S. holidays. ")
+if answer == "New Year’s Day":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect!")
+
+print("you got " + str(score) + " questions correct!")
+print("you got " + str((score/ 100) * 100) + "%")
+print("thank you for playing")
+
+
+
+
+
+
+
+
+
+
+
+
+
